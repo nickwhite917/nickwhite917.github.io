f1a745d4a4789a50eefb779e80a3625b645fac8e
nickwhite917 <nickwhite917@yahoo.com>
2016-04-19 09:11:30 -0400

    Added files initial

---
+++ files/send-email.php
@@ -0,0 +1,57 @@
+<?php 
+ $name=$_POST["name"];
+ $from=$_POST["email"];
+ $msg=$_POST["message"];
+ $receiver="nick@nickwhite.us";
+ $subject="Message from nickwhite.us";
+
+$message = "
+<html>
+<head>
+<title>MyWALL Contact</title>
+</head>
+<body>
+<table width='50%' border='0' align='center' cellpadding='0' cellspacing='0'>
+  <tr>
+    <td colspan='2' align='center' valign='top'></td>
+  </tr>
+  <tr>
+    <td width='50%' align='right'>&nbsp;</td>
+    <td align='left'>&nbsp;</td>
+  </tr>
+  <tr>
+    <td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>Name:</td>
+    <td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$name."</td>
+  </tr>
+  <tr>
+    <td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>Email:</td>
+    <td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$from."</td>
+  </tr>
+  <tr>
+    <td align='right' valign='top' style='border-top:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>Message:</td>
+    <td align='left' valign='top' style='border-top:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".nl2br($msg)."</td>
+  </tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <'.$from.'>' . "\r\n";
+
+
+   if(mail($receiver,$subject,$message,$headers))  
+   {
+      echo "<p class='pad'>The message has been sent!</p>";
+   }
+   else
+   {
+      echo "<p class='pad'>The message could not been sent!</p>";
+   }
+
+
+?>
